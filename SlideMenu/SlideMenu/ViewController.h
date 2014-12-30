cdd2f1a97295b355a50372b5e4c85153254812b9
hunghanam <levanhung.hus@gmail.com>
2014-12-30 09:35:04 +0700

    Slide menu IOS support IOS 7.0 or lastest
    Write in objective c. Slide menu include container view and combine
    UIView animation. it is same Facebook application

---
+++ SlideMenu/SlideMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideMenu
+//
+//  Created by HungLV on 30/12/2014.
+//  Copyright (c) Năm 2014 jp.co.enterAsia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SlideMenu/SlideMenu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SlideMenu
+//
+//  Created by HungLV on 30/12/2014.
+//  Copyright (c) Năm 2014 jp.co.enterAsia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
